5e52e8825f1b8d7b455b22250618e1e0fb5b95be
NRyoichi <95951809+NRyoichi.noreply.github.com>
2022-07-25 21:47:32 +0900

    メインの投稿用のロジックを実装

---
+++ src/component/Contribute.tsx
@@ -0,0 +1,40 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import { FC, useEffect, useState } from "react";
+import { db } from "../firebase";
+const Contribute: FC = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      message: "",
+      postedImage: "",
+      timestamp: null,
+      userImage: "",
+      username: "",
+    },
+  ]);
+
+  useEffect(() => {
+    const sortedQuery = query(
+      collection(db, "posts"),
+      orderBy("timestamp", "desc")
+    );
+    const getQuery = onSnapshot(sortedQuery, (snapShot) => {
+      setPosts(
+        snapShot.docs.map((doc) => ({
+          id: doc.id,
+          message: doc.data().message,
+          postedImage: doc.data().postedImage,
+          timestamp: doc.data().timestamp,
+          username: doc.data().username,
+          userImage: doc.data().userImage,
+        }))
+      );
+    });
+    return () => {
+      getQuery();
+    };
+  }, []);
+  return <div></div>;
+};
+
+export default Contribute;
