b12dea6beb25b489bff80fc4c3c985f4e7194b45
NRyoichi <95951809+NRyoichi.noreply.github.com>
2022-07-25 21:03:09 +0900

    React,Redux初期設定

---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import "firebase/storage";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = getFirestore();
+export const storage = getStorage();
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
